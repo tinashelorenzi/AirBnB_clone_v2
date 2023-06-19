a3242d8e417f971368bc12d7f980f844840874d6
Tinashe <tinashelorenzi@protonmail.com>
2023-06-19 13:11:39 +0200

    filestorage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,14 +38,6 @@ class FileStorage:
         else:
             return self.__objects
 
-    def new(self, obj):
-        """sets __object to given obj
-        Args:
-            obj: given object
-        """
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            self.__objects[key] = obj
 
     def save(self):
         """serialize the file path to JSON file path
@@ -55,6 +47,15 @@ class FileStorage:
             my_dict[key] = value.to_dict()
         with open(self.__file_path, 'w', encoding="UTF-8") as f:
             json.dump(my_dict, f)
+            
+    def new(self, obj):
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def reload(self):
         """serialize the file path to JSON file path
@@ -67,14 +68,14 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-    def delete(self, obj=None):
+    def close(self):
+        """ calls reload()
+        """
+        self.reload()
+        
+def delete(self, obj=None):
         """ delete an existing element
         """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
-
-    def close(self):
-        """ calls reload()
-        """
-        self.reload()
